919e8263c6cb5dd3f6c5d214d2872a9154cb73e0
Luan Luz <luanluz@MacBook-Air-de-Luan.local>
2024-06-04 20:31:28 -0300

    Column size 80

--- init.lua
+++ init.lua
@@ -107,6 +107,9 @@ vim.opt.relativenumber = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
+-- Enable column marker for 80 characters
+vim.opt.colorcolumn = '80'
+
 -- Don't show the mode, since it's already in status line
 vim.opt.showmode = false
 
